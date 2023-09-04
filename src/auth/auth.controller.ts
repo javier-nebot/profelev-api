bf77673a5fc6935cf38d1b2e00115c537b6f3147
Javier NebotAlminana <javier.nebot@gmail.com>
2023-09-04 14:22:39 +0200

    Signup Prof & tuteur

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,27 +2,34 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
 import { AuthEntity } from './entity/auth.entity';
-import { LoginDto } from './dto/login.dto';
+import { AuthDto } from './dto/auth.dto';
+import { CreateProfDto } from 'src/prof/dto/create-prof.dto';
+import { CreateTuteurDto } from 'src/tuteur/dto/create-tuteur.dto';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup') 
-  signup() {
-    return this.authService.signup();
+  @Post('signupProf') 
+  async signupProf(@Body() newProf: CreateProfDto) {
+    return this.authService.signupProf(newProf)
+  }
+
+  @Post('signupTuteur') 
+  async signupTuteur(@Body() newTuteur:  CreateTuteurDto) {
+    return this.authService.signupTuteur(newTuteur);
   }
 
   @Post('loginProf')
   @ApiOkResponse({ type: AuthEntity})
-  loginProf(@Body() {email, password}: LoginDto) {
+  loginProf(@Body() {email, password}: AuthDto) {
     return this.authService.loginProf(email, password);
   }
 
   @Post('loginTuteur')
   @ApiOkResponse({ type: AuthEntity})
-  loginTuteur(@Body() {email, password}: LoginDto) {
+  loginTuteur(@Body() {email, password}: AuthDto) {
     return this.authService.loginTuteur(email, password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,70 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthEntity } from './entity/auth.entity';
+import { AuthDto } from './dto/auth.dto'
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypy from 'bcrypt'
+import * as bcrypt from 'bcrypt'
+import { ProfService } from 'src/prof/prof.service';
+import { TuteurService } from 'src/tuteur/tuteur.service';
+import { CreateProfDto } from 'src/prof/dto/create-prof.dto';
+import { CreateTuteurDto } from 'src/tuteur/dto/create-tuteur.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService, private jwtService: JwtService) {}
-    signup() {
-        return {msg: 'I have signed up'};
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService,
+        private profService: ProfService,
+        private tuteurService: TuteurService) {}
+
+    public async signupProf(registrationProf: CreateProfDto) {
+        try {
+            const hashedPassword = await bcrypt.hash(
+                registrationProf.password,
+                10,
+            )
+            const createdProf = await this.profService.create({
+                ...registrationProf,
+                password: hashedPassword,
+            })
+            return createdProf
+        } catch (error) {
+            if (error?.code === '23505') {
+                throw new HttpException(
+                    'Prof avec cet email, exist',
+                    HttpStatus.BAD_REQUEST,
+                )
+            }
+            throw new HttpException(
+                `error: ${error}`,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            )
+        }
+    }
+
+    public async signupTuteur(registrationTuteur: CreateTuteurDto) {
+        try {
+            const hashedPassword = await bcrypt.hash(
+                registrationTuteur.password,
+                10,
+            )
+            const createdTuteur = await this.tuteurService.create({
+                ...registrationTuteur,
+                password: hashedPassword,
+            })
+            return createdTuteur
+        } catch (error) {
+            if (error?.code === '23505') {
+                throw new HttpException(
+                    'Tuteur avec cet email, exist',
+                    HttpStatus.BAD_REQUEST,
+                )
+            }
+            throw new HttpException(
+                `error: ${error}`,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            )
+        }
     }
 
     async loginProf(email: string, password: string): Promise<AuthEntity> {
@@ -16,7 +72,7 @@ export class AuthService {
         if (!prof) {
             throw new NotFoundException(`No user found for email: ${email}`)
         }
-        const isPassworValid = await bcrypy.compare(password, prof.password);
+        const isPassworValid = await bcrypt.compare(password, prof.password);
         if(!isPassworValid) {
             throw new UnauthorizedException('Invalid password');
         }
